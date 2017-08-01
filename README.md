e52e9f96d18be09242692190bb44e035b0f43788
case2912 <shiroyama@maia.eonet.ne.jp>
2017-08-01 18:51:37 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Grimoire.js勉強会#1
+
+> 基礎編のスライドに対応するサンプルです。
+> ローカルサーバーを起動することでサンプルを確認できます。
+
+```
+$ npm install -g http-server
+$ hs
+Starting up http-server, serving ./
+Available on:
+  http://127.0.0.1:8080
+  http://10.0.1.105:8080
+Hit CTRL-C to stop the server
+```
+
+#### https://grimoire.gl/
---
+++ goml_example/index.js
@@ -0,0 +1,16 @@
+gr.registerComponent('Rotate', {
+  attributes: {
+    speed: {
+      default: '1,1,1',
+      converter: 'Vector3'
+    }
+  },
+  $mount: function() {
+    this.phi = gr.lib.math.Vector3.Zero
+    this.delta = this.getAttribute('speed').multiplyWith(Math.PI / 180);
+  },
+  $update: function() {
+    this.phi = this.phi.addWith(this.delta)
+    this.node.setAttribute('rotation', gr.lib.math.Quaternion.eulerXYZ(this.phi.X, this.phi.Y, this.phi.Z))
+  }
+});
